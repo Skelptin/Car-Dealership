8ea434487b0979b7a93f63cbac8928d53d96fe97
Mayank Painkra <mpainkra7@gmail.com>
2024-02-29 16:06:39 +0530

    setup4

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,14 +1,31 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+// import adapter from "@sveltejs/adapter-auto";
+// import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+// 		// csrf: {
+// 		// 	checkOrigin: false,
+// 		// }
+// 	},
+
+// 	preprocess: [vitePreprocess({})],
+// };
+
+
+
+import cloudflare from '@sveltejs/adapter-cloudflare'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: cloudflare(),
+
+		target:"#svelte"
 		// csrf: {
 		// 	checkOrigin: false,
 		// }
 	},
+}
 
-	preprocess: [vitePreprocess({})],
-};
 export default config;
